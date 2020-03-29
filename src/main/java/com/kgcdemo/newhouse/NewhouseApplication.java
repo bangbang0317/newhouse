43edfd897ed639cd3d2fd86a28286a5f96b600bd
bangbang0317 <101821838@qq.com>
2020-03-29 09:53:08 +0800

    Initial commit

---
+++ src/main/java/com/kgcdemo/newhouse/NewhouseApplication.java
@@ -0,0 +1,13 @@
+package com.kgcdemo.newhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kgcdemo/newhouse/NewhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgcdemo.newhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
